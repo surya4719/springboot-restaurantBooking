f770d5fd9f948b659216a99bf306812a877e8458
surya <41474693+surya4719@users.noreply.github.com>
2021-07-27 16:39:39 +0530

    Add files via upload

--- Database.sql
+++ Database.sql
@@ -8,14 +8,6 @@ INSERT INTO reservation VALUES
 (1,100,2,800), 
 (2,101,3,800), 
 (3,102,5,800);
-drop table reservation;
-create table reservation(id int(1) NOT NULL AUTO_INCREMENT primary key,
-user_id int(1) NOT NULL,party_size tinyint(1) NOT NULL,
-restaurant_id int(1) NOT NULL);
-INSERT INTO reservation VALUES 
-(1,100,2,800), 
-(2,101,3,800), 
-(3,102,5,800);
-drop table reservation;
+select * from reservation;
 
 
